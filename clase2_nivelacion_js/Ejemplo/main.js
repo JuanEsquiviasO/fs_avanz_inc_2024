5ba655a607f61a49d8dae73cf353521ad1276e55
Juan Esquivias <juanesquiviasotazu@gmail.com>
2024-06-11 15:19:22 -0500

    created logig for delete data with button eliminar

--- clase2_nivelacion_js/Ejemplo/main.js
+++ clase2_nivelacion_js/Ejemplo/main.js
@@ -8,11 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
 /**
  * Función para agregar una nueva mascota a la lista.
  */
+
 function agregarMascota(){
 //Obtenemos el valor del input 'nuevaMascota' y eliminamos espacios en blanco
 	const nombreMascota = document.getElementById('nuevaMascota').value.trim()
 
-	if (nombreMascota ==''){
+	if (nombreMascota == ''){
 		alert('Por favor ingrese el nombre de la mascota')
 		return
 	}
@@ -22,6 +23,27 @@ function agregarMascota(){
 	nuevaMascota.classList.add('mascota')
 
 document.getElementById('listaMascotas').appendChild(nuevaMascota)
+
+
+// Crear un boton para eliminar mascota ya pintada
+const botonEliminar = document.createElement('button')
+//Asignar el texto 'Eliminar' al boton
+botonEliminar.textContent = 'Eliminar'
+
+botonEliminar.onclick = function () {
+	eliminarMascota(nuevaMascota)
 }
 
+//Añadir el boton Eliminar como hijo del elemento li 
+nuevaMascota.appendChild(botonEliminar)
 
+document.getElementById('listaMascota').appendChild(nuevaMascota)
+
+//limpiar campo de entrada despues de agregar mascota
+document.getElementById('nuevaMascota').value = ''
+}
+
+// Función para eliminar una mascota
+function eliminarMascota(mascota){
+  mascota.remove();
+}
